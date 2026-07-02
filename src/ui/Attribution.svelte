6db47339383fd4d7c9776b8a064e22362200a020
christoph <contact@because-why-not.com>
2026-07-02 17:03:15 +1200

    changed attribution to "Map data by"

--- src/ui/Attribution.svelte
+++ src/ui/Attribution.svelte
@@ -6,7 +6,7 @@
 
 {#if attribution}
     <div class="mx-3 mb-2 p-2 rounded bg-base-100 text-xs leading-5">
-        <div>Provided by <span class="font-semibold">{attribution.provider}</span></div>
+        <div>Map data by <span class="font-semibold">{attribution.provider}</span></div>
         <div><a class="link link-primary" href={attribution.homepage.url} target="_blank" rel="noopener noreferrer">{attribution.homepage.text}</a></div>
         <div>License: <a class="link link-primary" href={attribution.license.url} target="_blank" rel="noopener noreferrer">{attribution.license.text}</a></div>
         <div class="mt-1 pt-1 border-t border-base-300 opacity-70 whitespace-pre-line">{#each attribution.credit as span}{#if typeof span === 'string'}{span}{:else}<a class="link" href={span.url} target="_blank" rel="noopener noreferrer">{span.text}</a>{/if}{/each}</div>
